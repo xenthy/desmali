4d5bd9b229247aab9f08d3def33d321a2876c380
seymouslee <seymouslee@yahoo.com.sg>
2021-03-16 02:19:45 +0800

    whitelist methods added to goto_injector

--- src/desmali/obfuscate/restructure/goto_injector.py
+++ src/desmali/obfuscate/restructure/goto_injector.py
@@ -1,8 +1,8 @@
-from desmali.extras import logger, Util
+from desmali.extras import logger, Util, regex
 from desmali.tools import Dissect
 
-START_GOTO = "\n\tgoto :desmaili_back \n\n\t:desmaili_front\n\n"
-END_GOTO = "\n\t:desmaili_back\n\n\tgoto :desmaili_front\n\n"
+START_GOTO = "\n    goto :desmaili_back \n\n    :desmaili_front\n\n"
+END_GOTO = "\n    :desmaili_back\n\n    goto :desmaili_front\n\n"
 
 
 class GotoInjector:
@@ -21,12 +21,17 @@ class GotoInjector:
                                           description=f"Injecting goto(s): "):
             logger.debug(f"modifying \"{filename}\"")
             is_started: bool = False
+            white_list = []
+            
 
             with Util.inplace_file(filename) as file:
 
+                white_list = self.methodLabels(file)
+
                 for line in file:
-                    # check for start of method
-                    if ".method" in line and "abstract" not in line:
+                    # check for start of method in whitelist
+                    #if ".method" in line and "abstract" not in line:
+                    if any(mtd in line for mtd in white_list):
                         file.write(line)
                         file.write(START_GOTO)
                         is_started = True
@@ -39,3 +44,37 @@ class GotoInjector:
 
                     else:
                         file.write(line)
+
+    def methodLabels(self, file):
+
+        in_method: bool = False
+        curr_method: str = ""
+        white_list = []
+
+        for line in file:
+
+            # check for the start of a method that is not abstract or a constructor
+            if regex.METHOD.match(line) and "abstract" not in line and "constructor" not in line:
+                in_method = True
+                cm = line.split("(")
+                curr_method = cm[0]
+            
+            # check for if there is a label in the constructor
+            # if yes, the method will be added to the whitelist
+            elif in_method == True and regex.LABEL.match(line):
+                white_list.append(curr_method)
+                curr_method = ""
+
+            # check for the end of the method
+            elif ".end method" in line and in_method:
+                in_method = False
+        
+            file.write(line)
+        
+        # remove any null elements and duplicates in whitelist
+        white_list = filter(None, white_list)
+        white_list = list(set(white_list))
+
+        return white_list
+
+
