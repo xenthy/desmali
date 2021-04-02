d8db0e058701709f848ab08277166eccd7d92165
xenthy <21235891+xenthy@users.noreply.github.com>
2021-04-02 10:25:35 +0800

    added a diff tool to view difference between original and modified files
    Signed-off-by: xenthy <21235891+xenthy@users.noreply.github.com>

--- src/desmali/tools/__init__.py
+++ src/desmali/tools/__init__.py
@@ -1,5 +1,6 @@
 from .apksigner import Apksigner
 from .apktool import Apktool
 from .dex2jar import Dex2jar
+from .diff import Diff
 from .dissect import Dissect
 from .zipalign import Zipalign
--- src/desmali/tools/diff.py
+++ src/desmali/tools/diff.py
@@ -1,4 +1,12 @@
-from difflib import make_file
+from difflib import HtmlDiff
+
 
 class Diff:
-    pass
+    def __init__(self, output_path: str):
+        self._output_path = output_path
+
+    def generate_diff(self, original_list, modified_list):
+        original = open(original_list[0], "r").readlines()
+        modified = open(modified_list[0], "r").readlines()
+        with open("diff.html", "w+") as file:
+            file.write(HtmlDiff().make_file(original, modified, context=True))
